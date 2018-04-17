a82f728c4f3355b6e5cc747f14d6c64a750e370d
Jeamay Palo <jeamay.palo@gmail.com>
2018-04-17 14:04:03 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", "3", "M", "brown", "jolly", );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", "3", "M", "brown", "jolly", "0");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ();
